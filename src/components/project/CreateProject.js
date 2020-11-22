5a58e867591b03289355d23cdcefdd29288e60f1
Cuong <cuongnvse04982@gmail.com>
2020-11-22 17:32:41 +0700

    add crateProject action

--- src/components/project/CreateProject.js
+++ src/components/project/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { createProject } from '../../store/actions/projectActions';
 
 export class CreateProject extends Component {
 
@@ -9,7 +11,9 @@ export class CreateProject extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    const project = this.state;
+    const {createProject} = this.props;
+    createProject(project);
   }
 
 
@@ -41,4 +45,10 @@ export class CreateProject extends Component {
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  };
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
